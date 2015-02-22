2045b9e64b5ce152736ec4c36de72b946d7358f8
Jonas Andersson <jonas123@gmail.com>
2015-02-21 19:52:56 -0600

    Create unity test main()

---
+++ Lab1/test/AllTests.c
@@ -0,0 +1,11 @@
+#include "unity_fixture.h"
+
+static void RunAllTests(void)
+{
+    RUN_TEST_GROUP(myHead);
+}
+
+int main(int argc, char * argv[])
+{
+    return UnityMain(argc, argv, RunAllTests);
+}
