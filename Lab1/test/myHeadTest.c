f2ccc8dd006f6fa8469ed7553cd8c51cb7b19ac1
Jonas Andersson <jonas123@gmail.com>
2015-02-21 19:49:34 -0600

    Create unity test main()

--- Lab1/test/myHeadTest.c
+++ Lab1/test/myHeadTest.c
@@ -1,4 +1,4 @@
-#include "unity.h"
+#include "unity_fixture.h"
 
 TEST_GROUP(myHead);
 
---
+++ Lab1/test/myHeadTestGroupRunner.c
@@ -0,0 +1,5 @@
+#include "unity_fixture.h"
+TEST_GROUP_RUNNER(myHead)
+{
+    RUN_TEST_CASE(myHead, AcceptOption_n);
+}
