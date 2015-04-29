91d55117b61e4e8621a4b7230f9d13ea85079674
Jonas Andersson <jonas123@gmail.com>
2015-04-29 18:21:17 -0500

    Create server program

--- lab4/lab4-server.c
+++ lab4/lab4-server.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+int main(void)
+{
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
+    if (bind() == -1){
+        perror("Could not create socket");
+        exit(EXIT_FAILURE);
+    }
+    listen();
+    accept();
+    exit(EXIT_SUCCESS);
+}
