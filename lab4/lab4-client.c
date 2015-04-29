7ee17d89a56da2c4aae148ebd36f4e730543be7c
Jonas Andersson <jonas123@gmail.com>
2015-04-29 18:21:03 -0500

    Add skeleton function calls to setup a client TCP/IP program

--- lab4/lab4-client.c
+++ lab4/lab4-client.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
 
 int main(void)
 {
-    printf("Hello World!\n");
-    return 0;
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    connect();
+    exit(EXIT_SUCCESS);
 }
 
