d39f5ce55064ac793e1f0e6cebc4a90119847387
Jonas Andersson <jonas@andersson.io>
2015-05-01 18:40:32 -0500

    Bind socket

--- lab4/lab4-server.c
+++ lab4/lab4-server.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define PORT 3060
+
 int main(void)
 {
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    if (bind() == -1){
+    // Create Socket
+    int sockfd;
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+        perror("Could not create socket");
+        exit(EXIT_FAILURE);
+    }
+
+    // Bind Socket
+    struct sockaddr_in servaddr;
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(PORT);
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if (bind(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) == -1){
         perror("Could not create socket");
         exit(EXIT_FAILURE);
     }
