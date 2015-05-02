5bb7876d4bb3d715c5d640e345a24ab1dd4a90ee
Jonas Andersson <jonas@andersson.io>
2015-05-02 18:05:08 -0500

    Switch to home directory on server startup

--- lab4/lab4-server.c
+++ lab4/lab4-server.c
@@ -8,7 +8,7 @@
 #include <linux/limits.h>
 
 #define PORT 3060
-#define SECRET "CS30615spr"
+#define SECRET "<CS30615spr>\n"
 
 /* Remcp Protocol
 
@@ -18,24 +18,38 @@
 
 */
 const char * remcp = "<remcp>\n";
-const char * secret = "<JAA>\n";
+const char * secret = SECRET;
 const char * ok = "<ok>\n";
 const char * ready = "<ready>\n";
 const char * protocol_send = "<send>\n";
 const char * error = "<error>\n";
 
+int validate_filepath(char * file_path);
+
+int validate_filepath(char * file_path){
+
+}
+
 int main(void)
 {
     char message_buffer[PATH_MAX + 1];
-    // Create Socket
+    struct sockaddr_in servaddr;
     int sockfd, connection_fd, nread;
+
+    // Change to home directory
+    if (chdir(getenv("HOME")) == -1){
+        perror("Unable to switch to home directory");
+        exit(EXIT_FAILURE);
+    }
+
+    // Create Socket
     if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
         perror("Could not create socket");
         exit(EXIT_FAILURE);
     }
 
     // Bind Socket
-    struct sockaddr_in servaddr;
+
     memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(PORT);
@@ -56,14 +70,30 @@ int main(void)
     while(1){
         // Complete connection with listening socket, disregarding client address data
         connection_fd = accept(sockfd, (struct sockaddr *) NULL, NULL);
+
+        // Read <remcp>\n
         nread = read(connection_fd, message_buffer, sizeof(message_buffer));
         message_buffer[nread] = '\0';
         printf("Client says: %s\n", message_buffer);
-        printf("strcmp:%i\n", strcmp(message_buffer, remcp));
         if(strcmp(message_buffer, remcp) == 0){
             write(connection_fd, remcp, strlen(remcp));
         }
         else close(connection_fd);
+
+        // Read <secret>\n
+        nread = read(connection_fd, message_buffer, sizeof(message_buffer));
+        message_buffer[nread] = '\0';
+        printf("Client says: %s\n", message_buffer);
+        if(strcmp(message_buffer, secret) == 0){
+            write(connection_fd, ok, strlen(ok));
+        }
+        else close(connection_fd);
+
+        // Read file pathname
+
+        nread = read(connection_fd, message_buffer, sizeof(message_buffer));
+        message_buffer[nread] = '\0';
+        printf("Client says: %s\n", message_buffer);
 
         close(connection_fd);
     }
